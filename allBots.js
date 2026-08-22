797be7a3b121b16c6074c323b41c57fa4ea8f177
MatBou314 <math.bouchard10@gmail.com>
2026-08-22 11:37:07 -0400

    Update allBots.js

--- allBots.js
+++ allBots.js
@@ -1,6 +1,4 @@
 import { botList1 } from "./bot.js";
 import { botList2 } from "./bot2.js";
-import { botListGemini } from "./geminiBot.js";
-import { botListDeepSeek } from "./deepSeekBot.js";
 
 export const botList = {...botList1,...botList2}
